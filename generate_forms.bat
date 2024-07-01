6db9aa54e753f3f51f8378b1bf03c4c286f753f0
unknown <julietoompad1993@gmail.com>
2024-07-01 11:45:27 +0800

    Okay!!!

--- generate_forms.bat
+++ generate_forms.bat
@@ -1,7 +1,12 @@
 @echo off
-set /p PREFIX=Enter PREFIX:
-set /p START=Enter START:
-set /p END=Enter END:
+echo =============================================
+echo + WELCOME TO BPSF CLAIM STUB FORM GENERATOR +
+echo =============================================
+echo.
+
+set /p PREFIX=Enter (Form Prefix):
+set /p START=Enter (Start):
+set /p END=Enter (End):
 
 php index.php Tpdf bpsf %PREFIX% %START% %END%
 
